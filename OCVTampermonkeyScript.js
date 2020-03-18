c63935ebccde5a3bfaa7c91ad453a94d1608df9b
smallbone <lue2048@163.com>
2020-03-18 17:28:39 +0800

    check the min and max minitues and adjust the tool css style; add tool to the page depend on URL

--- OCVTampermonkeyScript.js
+++ OCVTampermonkeyScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OCV review case tool
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  shortcut for review case
 // @author       Bruce Lu
 // @include      https://ocv.microsoft.com/*
@@ -37,21 +37,16 @@ let savingCircleSelector =
   "body > div:nth-child(49) > div > div.view-port > div > div.triage.container-fluid > div > div.item-detail-pane.col-xs-8 > div > item-details > div > div > div > div:nth-child(3) > div.tab-content > div > div.review-notes-section > div.review-notes-saving-spinner";
 let updateTimeStampSelector =
   "body > div:nth-child(49) > div > div.view-port > div > div.triage.container-fluid > div > div.item-detail-pane.col-xs-8 > div > item-details > div > div > div > div:nth-child(3) > div.tab-content > div > div:nth-child(2)";
+let ribbonSelector =
+  "body > div:nth-child(49) > div > div.view-port > div > triage-ribbon > div.ribbon";
 
 let MIN = 4;
 let MAX = 5;
 let isShortcut = false;
+let alias;
 let issuesStatusList;
 let reviewNotesSection;
 
-var style = document.createElement("style");
-style.type = "text/css";
-var text = document.createTextNode(`
-
-`);
-style.appendChild(text);
-var head = document.getElementsByTagName("head")[0];
-head.appendChild(style);
 
 function formateDate(splitor) {
   splitor = splitor || "/";
@@ -65,10 +60,19 @@ function formateDate(splitor) {
 }
 
 function getAlias() {
-  let myFeedbackLink = document.querySelector(myFeedbackLinkSelector);
-  let str = decodeURI(myFeedbackLink.href);
-  let alias = str.match(/([\w-]+)@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/)[1];
-  return alias;
+  return new Promise((resolve, reject) => {
+    $(document).arrive(myFeedbackLinkSelector, function() {
+      let myFeedbackLink = $(this)[0];
+      let str = decodeURI(myFeedbackLink.href);
+      alias = str.match(/([\w-]+)@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/)[1];
+      console.log("Get alias: ", alias)
+      if (alias.length > 3) {
+        resolve(alias)
+      } else {
+        reject("cannot get the alias")
+      }
+    })
+  })
 }
 
 function getRandomIntInclusive(min, max) {
@@ -95,7 +99,6 @@ function getIssuesStatus() {
 
 function addNotes(node) {
   return new Promise((resolve, reject) => {
-    let alias = getAlias();
     let date = formateDate();
     let reivewNotesTime = getRandomIntInclusive(MIN, MAX);
     // Get the elements
@@ -106,6 +109,7 @@ function addNotes(node) {
       triagingSuggestionsList[triagingSuggestionsList.length - 1];
     // Fill the reivew notes
     if (node.value === "") {
+      console.log("Add review notes...")
       let reviewNotes = `${date}, ${alias}, ${reivewNotesTime}:\n${alias}, ${issuesText}`;
       node.value = reviewNotes;
       node.dispatchEvent(new Event("change"));
@@ -118,41 +122,59 @@ function addNotes(node) {
 
 function changeReviewNotesTime(e) {
   e.stopPropagation()
+  e.preventDefault()
+  let min = document.getElementById("min-time").value || MIN;
+  let max = document.getElementById("max-time").value || MAX;
   const timeController = document.getElementById("notes-time-controller")
-  MIN = document.getElementById("min-time").value || MIN;
-  MAX = document.getElementById("max-time").value || MAX;
-  alert(`Notes time has changed between ${MIN} - ${MAX} `)
+  if (parseInt(min) > parseInt(max)) {
+    alert(`Notes time invalid, max value must lager than min value. `)
+    return
+  } else {
+    MIN = min;
+    MAX = max;
+    alert(`Notes time has changed between ${MIN} - ${MAX} `)
+  }
+}
+
+function matchedURL(keyword) {
+  let hash = location.hash
+  return hash.startsWith(keyword) ? true : false
 }
 
 
 // Add util tool element in the page
-function addUtilTool() {
-  $(document).arrive(userNameSelector, { onceOnly: true }, function() {
-    console.log("Add util tool to the page");
-    let userNameEle = $(this)[0];
+async function addUtilTool() {
+  if (matchedURL("#/triage/")) {
+    await getAlias()
+    $(document).arrive(ribbonSelector, { onceOnly: true }, function() {
+      console.log("Add util tool to the page");
+      let ribbonEle = $(this)[0];
 
-    let myDailReviewedURL = `https://ocv.microsoft.com/#/discover/?searchtype=OcvItems&relDateType=all&offset=0&q=(Product:"SharePoint" OR Product:"OneDrive for Business" OR Product:"Outlook" OR Product:"Azure AD") AND (OcvAreas:(SetDate:${formateDate(
-      "-"
-    )} AND (SetBy:"${getAlias()}")))&allAreas`;
-    let ultilHTML = `
-<div id='util-container' style="position: fixed; width: 250px; top: 90px; right: 20px; overflow: hidden; z-index: 9999">
-	<div class="daily-reviewed">
-		<a href=${encodeURI( myDailReviewedURL )} target='_blank' onclick="event.stopPropagation();"
-			style="background-color: #005A9E; color: white; display: inline-block;">DailyReviewed</a>
-	</div>
-	<div id="notes-time-controller">
-    <div>Notes Time</div>
-		<input type="number" name="min" class="form-control" id="min-time" onclick="event.stopPropagation();" placeholder="min" style="display: inline; width: 30%;">
-		<input type="number" name="max" class="form-control" id="max-time" onclick="event.stopPropagation();" placeholder="max" style="display: inline; width: 30%;">
-		<button type="button" class="btn btn-primary" id="apply-btn">Apply</button>
-	</div>
-</div>
+      let myDailReviewedURL = `https://ocv.microsoft.com/#/discover/?searchtype=OcvItems&relDateType=all&offset=0&q=OcvAreas:(SetDate:${formateDate("-")} AND (SetBy:"${alias}"))&allAreas`;
+      let ultilHTML = `
+  <div id='util-container' style="position: fixed; width: 250px; top: 90px; right: 20px; overflow: hidden; z-index: 9999">
+    <div class="daily-reviewed">
+      <a href=${encodeURI( myDailReviewedURL )} target='_blank' onclick="event.stopPropagation();"
+        style="background-color: #005A9E; color: white; display: inline-block;">DailyReviewed</a>
+    </div>
+    <div id="notes-time-controller" style="margin-top: 10px;">
+      <p style="margin: 0;">Notes Time</p>
+      <form>
+        <input type="number" name="min" class="form-control" id="min-time" onclick="event.stopPropagation();" placeholder="min" value=${MIN} style="display: inline-block; width: 30%;" min="1" max="8">
+        <input type="number" name="max" class="form-control" id="max-time" onclick="event.stopPropagation();" placeholder="max" value=${MAX} style="display: inline-block; width: 30%;" min="1" max="8">
+        <button type="button" class="btn btn-primary" id="apply-btn" style="margin-top: -5px;">Apply</button>
+      </form>
+    </div>
+  </div>
 
-    `;
-    userNameEle.insertAdjacentHTML("beforeend", ultilHTML);
-    let applyBtn = document.getElementById("apply-btn");
-    applyBtn.addEventListener("click", changeReviewNotesTime)
-  });
+      `;
+      ribbonEle.insertAdjacentHTML("beforeend", ultilHTML);
+      let applyBtn = document.getElementById("apply-btn");
+      applyBtn.addEventListener("click", changeReviewNotesTime)
+    });
+
+
+  }
 }
 
 
@@ -167,12 +189,13 @@ function reviewCase(e) {
       }
       isShortcut = true;
       reviewNotesSection.click();
+      console.log("Saving the review notes...")
     }
 }
 
 // Waiting for reivew notes section element loaded
 $(document).arrive(reviewNotesSectionSelector, function() {
-  console.log("init the ocv review notes auto fill tool...");
+  console.log("init the ocv review notes tool...");
   init();
 });
 
@@ -206,7 +229,7 @@ function init() {
   document.onkeydown = reviewCase
 }
 
-addUtilTool();
+addUtilTool()
 
 ]]></>).toString();
 var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
